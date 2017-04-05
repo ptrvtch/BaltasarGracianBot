a1bbd92e0e31acfefebc512cd4eb66c841a28fd6
Олег Омельченко < >
2017-04-06 01:01:43 +0300

    init

---
+++ bot.py
@@ -0,0 +1,38 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import datetime
+import logging
+import pprint
+import random
+
+
+def main():
+    quotes = open("baltasar.txt", 'r', encoding='utf8')
+    system_random = random.SystemRandom()
+    data = quotes.readlines()
+    updater = Updater(token='370834023:AAHV8HUYF2UN24DYAuLIDI3z9UXd52feqcM')
+
+    dispatcher = updater.dispatcher
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+        level=logging.INFO
+    )
+
+    def start(bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text="Добро пожаловать! Нажмите /quote чтобы получить цитату из книги")
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    def quote(bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text=system_random.choice(data))
+
+    quote_handler = CommandHandler('quote', quote)
+    dispatcher.add_handler(quote_handler)
+
+    updater.start_polling()
+    print('Bot is ready!')
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
