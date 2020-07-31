a4b7a5420666ca90a4c4f5d7a1fa8b41b5106139
Oleh Omelchenko <ptrvtch@gmail.com>
2020-07-31 15:56:49 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,43 @@ import os
 from telegram.ext import Updater, CommandHandler
 import logging
 import random
+import requests
+import dotenv
 
-bot_token = os.environ.get("bot_token")
+from dotenv import load_dotenv
+load_dotenv()
 
+bot_token = os.environ.get("BOT_TOKEN")
+GA_TRACKING_ID = os.environ.get("GA_TRACKING_ID")
 
 logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+def track_event(cid, category, action, label=None, value=0):
+    data = {
+        'v': '1',  # API Version.
+        'tid': GA_TRACKING_ID,  # Tracking ID / Property ID.
+        # Anonymous Client Identifier. Ideally, this should be a UUID that
+        # is associated with particular user, device, or browser instance.
+        'cid': cid,
+        't': 'event',  # Event hit type.
+        'ec': category,  # Event category.
+        'ea': action,  # Event action.
+        'el': label,  # Event label.
+        'ev': value,  # Event value, must be an integer
+        'ua': 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'
+    }
+
+    response = requests.post(
+        'https://www.google-analytics.com/collect', data=data)
+
+    response.raise_for_status()
+
 
 def main():
     quotes = open("baltasar.txt", "r", encoding="utf8")
@@ -16,15 +48,13 @@ def main():
 
     dispatcher = updater.dispatcher
 
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=logging.INFO,
-    )
 
     def start(update, context):
         logger.info("Start command")
+        chat_id = update.message.chat_id
+        track_event(update.message.chat_id, 'start', '(not set)')
         context.bot.sendMessage(
-            chat_id=update.message.chat_id,
+            chat_id=chat_id,
             text="Добро пожаловать! Нажмите /quote чтобы получить цитату из книги",
         )
 
@@ -33,16 +63,19 @@ def main():
 
     def quote(update, context):
         message = system_random.choice(data)
+        chat_id = update.message.chat_id
+        track_event(update.message.chat_id, 'quote', message[0:20])
         try:
             context.bot.sendMessage(
-                chat_id=update.message.chat_id, text=message, parse_mode="Markdown"
+                chat_id=chat_id, text=message, parse_mode="Markdown"
             )
             # logging.info(flatten_metadata(update.message))
+            logger.info(context)
             logger.info({"message": message[0:20]})
         except Exception as e:
             logger.warning(str(e))
             context.bot.sendMessage(
-                chat_id=update.message.chat_id, text=message, parse_mode="Markdown"
+                chat_id=chat_id, text=message, parse_mode="Markdown"
             )
 
     quote_handler = CommandHandler("quote", quote)
